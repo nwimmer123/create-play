89f514eab3803997b67139dff8509324fa13f18d
Noah Wimmer <nwimmer123@yahoo.com>
2016-02-19 13:20:09 -0800

    created game model

---
+++ db/migrate/20160219211947_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.text :title
+      t.integer :starting_choice_id
+      t.boolean :published
+      t.text :backstory
+
+      t.timestamps null: false
+    end
+  end
+end
