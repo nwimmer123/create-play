bd9dc11abb5d763e4cbc3fba1c31fc68a832e3c0
Noah Wimmer <nwimmer123@yahoo.com>
2016-02-19 15:49:40 -0800

    user creation stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,11 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def create
+    user_params = params.require(:user).permit(:name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to root_path
+  end
+
 end
