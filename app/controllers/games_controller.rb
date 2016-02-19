03285f76be3b2f89a57b123ef95845a84a93e804
Noah Wimmer <nwimmer123@yahoo.com>
2016-02-19 15:38:41 -0800

    added sign up page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,7 @@ class GamesController < ApplicationController
   def new
   end
 
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   root to: "users#index"
 
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
+
   get "/newgame", to: "choices#index"
-  
+
+  get "/newgame/backstory", to: "games#new"
+  post "/newgame/backstory", to: "games#create"
+
 
 end
