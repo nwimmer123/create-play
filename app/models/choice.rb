97d5964034fd047c6a3c0877c5f7717a30a4bfa1
Noah Wimmer <nwimmer123@yahoo.com>
2016-02-23 13:58:08 -0800

    partial relationship success

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,3 +1,10 @@
 class Choice < ActiveRecord::Base
   belongs_to :game
+
+  belongs_to :choiceA, class_name: "Choice", foreign_key: "choiceA_id"
+  belongs_to :choiceB, class_name: "Choice", foreign_key: "choiceB_id"
+
+  #has_one :parent_choice, class_name: "Choice"
+  has_one :parent_choice,  class_name: 'Choice', foreign_key: 'choiceA_id'
+  #has_one :parent_choice, 
 end
